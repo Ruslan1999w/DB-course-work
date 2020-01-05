46188a48c6f03b46e337d59f12465dfa9faf1d67
Ruslan Plaksunov <ruslan.plaxunov@yandex.ru>
2020-01-05 14:18:05 +0300

    version 1.0.2

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('book/<int:pk>/', views.book, name='book'),
+    path('registration/', views.registration, name='registration')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Book
+from .models import Book, Users
 
 
 def index(request):
@@ -11,3 +11,12 @@ def book(request, pk):
     post = Book.objects.filter(book_id=pk)
     context = {'posts': post}
     return render(request, 'blog/book.html', context)
+
+
+def registration(request):
+    Users.objects.create(
+        login=request.POST['login'],
+        email=request.POST['email'],
+        password=request.POST['password']
+    )
+    return render(request, 'blog/registration.html')
